d86b277bf7f1ae24bb5cad78c569ec4c3ad95577
WIN11 <WIN11@BYT-1907>
2026-06-18 22:08:54 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Yukarıdakiler kütüphanelerdir.
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.Write("Merhaba Dünya!");
+            //Console.WriteLine("\nMerhaba Güneş!");
+            //Console.WriteLine("Merhaba Ay!");
+
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+            //Console.WriteLine("");
+            //Console.WriteLine("1- Çorbalar");
+            //Console.WriteLine("2- Ana Yemekler");
+            //Console.WriteLine("3- Soğuk Başlangıçlar");
+            //Console.WriteLine("4- Salatalar");
+            //Console.WriteLine("5- Tatlılar");
+            //Console.WriteLine("6- İçicekler");
+            //Console.WriteLine("");
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+            #region Değişkenler
+            
+            //string customerName; // string veri tipi
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Ali";
+            //customerSurname = "İşler";
+            //customerPhone = "+90 543 621 85 26";
+            //customerEmail = "ali.isler@csharpdeneme.com";
+            //district = "Beykoz";
+            //city = "İstanbul";
+
+            //Console.WriteLine("***** Rezervasyon Kartı *****");
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("E-Mail Adresi: " + customerEmail);
+            //Console.WriteLine("İkametgah Adresi: " + district + " / " + city);
+            //Console.WriteLine("-----------------------------------------");
+
+            //customerName = "Ayşegül";
+            //customerSurname = "Güçlü";
+            //customerPhone = "+90 532 578 73 89";
+            //customerEmail = "aysegul.guclu@csharpdeneme.com";
+            //district = "Sarıyer";
+            //city = "İstanbul";
+
+
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("E-Mail Adresi: " + customerEmail);
+            //Console.WriteLine("İkametgah Adresi: " + district + " / " + city);
+            //Console.WriteLine("-----------------------------------------");
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+// Bu bir yorum satırıdır. 
