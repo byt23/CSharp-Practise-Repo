9b4dfb49b49471247ff5a7c6f709f5d5200f3264
BYT 1907 <https://github.com/byt23/CSharp-Practise-Repo>
2026-06-19 12:15:19 +0300

    # string and integer variable

--- 01_MainSubjects/Program.cs
+++ 01_MainSubjects/Program.cs
@@ -30,8 +30,8 @@ namespace _01_MainSubjects
 
             #endregion
 
-            #region Değişkenler
-            
+            #region stringDeğişkenler
+
             //string customerName; // string veri tipi
             //string customerSurname;
             //string customerPhone;
@@ -70,7 +70,71 @@ namespace _01_MainSubjects
             //Console.WriteLine("-----------------------------------------");
             #endregion
 
+            #region intDeğişkenler
+            // int
+            //int number = 24;
+            //Console.WriteLine("Sayı: " + number);
 
+            //int hamburgerPrice = 500;
+            //int cokePrice = 50;
+            //int waterPrice = 20;
+            //int friesPrice = 100;
+            //int pizzaPrice = 400;
+            //int lemonadePrice = 80;
+
+            //Console.WriteLine("***** Restoran Menü Fiyat Listesi *****");
+            //Console.WriteLine();
+            //Console.WriteLine("Hamburger fiyatı: " + hamburgerPrice + " TL");
+            //Console.WriteLine("Kola fiyatı: " +cokePrice + " TL");
+            //Console.WriteLine("Su fiyatı: " + waterPrice + " TL");
+            //Console.WriteLine("Kızartma fiyatı: " + friesPrice + " TL");
+            //Console.WriteLine("Pizza fiyatı: " + pizzaPrice + " TL");
+            //Console.WriteLine("Limonata fiyatı: " + lemonadePrice + " TL");
+            //Console.WriteLine("-----------------------------------------");
+            //Console.WriteLine();
+  
+
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice;
+            //int totalCokePrice;
+            //int totalWaterPrice;
+            //int totalFriesPrice;
+            //int totalPizzaPrice;
+            //int totalLemonadePrice;
+
+            //int totalPrice;
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokePrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalFriesPrice = friesCount * friesPrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalLemonadePrice = lemonadeCount * lemonadePrice;
+
+            //totalPrice = totalHamburgerPrice +totalCokePrice + totalWaterPrice + totalFriesPrice + totalPizzaPrice + totalLemonadePrice;
+
+            //Console.WriteLine("Toplam Hamburger tutarı: " + totalHamburgerPrice + " TL");
+            //Console.WriteLine("Toplam Kola tutarı: " + totalCokePrice + " TL");
+            //Console.WriteLine("Toplam Su tutarı: " + totalWaterPrice + " TL");
+            //Console.WriteLine("Toplam Kızartma tutarı: " + totalFriesPrice + " TL");
+            //Console.WriteLine("Toplam Pizza tutarı: " + totalPizzaPrice + " TL");
+            //Console.WriteLine("Toplam Limonata tutarı: " + totalLemonadePrice + " TL");
+            //Console.WriteLine("Toplam tutar: " + totalPrice + " TL");
+            #endregion
 
             Console.Read();
         }
