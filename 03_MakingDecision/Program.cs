f416dda40b4c87dd564d99a5761cf1e06fe676b0
BYT 1907 <https://github.com/byt23/CSharp-Practise-Repo>
2026-06-22 01:12:07 +0300

    day 3.2

--- 03_MakingDecision/Program.cs
+++ 03_MakingDecision/Program.cs
@@ -299,9 +299,116 @@ namespace _03_MakingDecision
             #endregion
 
             #region Switch Case
-            // 3. video 37:10
+
+            //Console.Write("Lütfen bir ay numarası giriniz: ");
+            //int monthNumber = int.Parse(Console.ReadLine());
+
+            //switch (monthNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Ocak");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Şubat");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Mart");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Nisan");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Mayıs");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Haziran");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Temmuz");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Ağustos");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Eylül");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Ekim");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Kasım");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Aralık");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Geçersiz ay numarası.");
+            //        break;
+            
+        #endregion
+
+            #region Switch Case Hesap Makinesi
+
+            //int number1, number2, result;
+            //char symbol;
+
+            //Console.Write("1. sayıyı giriniz: ");
+            //number1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("2. sayıyı giriniz: ");
+            //number2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("İşlem sembolünü giriniz (+, -, *, /, %, ^): ");
+            //symbol = char.Parse(Console.ReadLine());
+
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result = number1 + number2;
+            //        Console.WriteLine("Toplam: " + result);
+            //        break;
+            //    case '-':
+            //        result = number1 - number2;
+            //        Console.WriteLine("Fark: " + result);
+            //        break;
+            //    case '*':
+            //        result = number1 * number2;
+            //        Console.WriteLine("Çarpım: " + result);
+            //        break;
+            //    case '/':
+            //        if (number2 != 0)
+            //        {
+            //            result = number1 / number2;
+            //            Console.WriteLine("Bölüm: " + result);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Bir sayı sıfıra bölünemez.");
+            //        }
+            //        break;
+            //    case '%':
+            //        if (number2 != 0)
+            //        {
+            //            result = number1 % number2;
+            //            Console.WriteLine("Kalan: " + result);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Bir sayı sıfıra bölünemez.");
+            //        }
+            //        break;
+
+            //    case '^':
+            //        result = (int)Math.Pow(number1, number2);
+            //        Console.WriteLine("Üs: " + result);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Geçersiz işlem sembolü.");
+            //        break;
+            //}
 
             #endregion
+
             Console.Read();
         }
     }
